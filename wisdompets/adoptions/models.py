a237beac7d25cc1ff1916013f7bd39cf552ce510
Carlos Alberto Pozos Villegas <cpozosvillegas@hotmail.com>
2021-02-11 20:37:28 -0600

    Initial migration

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -4,7 +4,7 @@ class Pet(models.Model):
     SEX_CHOICES = [('M', 'Male'),('F', 'Female')]
     name = models.CharField(max_length=100)
     submitter = models.CharField(max_length=100)
-    species = model.CharField(max_length=50)
+    species = models.CharField(max_length=50)
     breed = models.CharField(max_length=50, blank=True)
     description = models.TextField()
     sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
